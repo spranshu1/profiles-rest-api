b60d94affc41fa1aae93933c634622c39704bbc8
Pranshu Shrivastava <pranshushrivastava20@gmail.com>
2020-09-16 01:34:32 +0530

    enable django admin for profiles api

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -3,4 +3,6 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from profiles_api import models
+
+admin.site.register(models.UserProfile)
