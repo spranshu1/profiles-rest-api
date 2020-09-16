39e6076264713a2ca417ed63190d6578b71ffa56
Pranshu Shrivastava <pranshushrivastava20@gmail.com>
2020-09-17 01:33:49 +0530

    Added viewset to profiles api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from profiles_api import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name = 'hello-viewset')
 
 urlpatterns = [
-	url(r'^hello-view/', views.HelloApiView.as_view())
+	url(r'^hello-view/', views.HelloApiView.as_view()),
+	url('', include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -18,7 +18,7 @@ class HelloApiView(APIView):
 
 	def get(self, request, format=None):
 		"""Returns a list of APIView Features"""
-		
+
 		an_apiview = [
 			'Uses HTTP methods as functions (get, post, patch, put, delete)',
 			'Is similar to a traditional Django View',
@@ -38,9 +38,9 @@ class HelloApiView(APIView):
 			return Response({'message':message})
 		else:
 			return Response(
-				serializer.errors, 
+				serializer.errors,
 				status=status.HTTP_400_BAD_REQUEST
-			)	
+			)
 
 
 	def put(self, request, pk=None):
@@ -53,6 +53,50 @@ class HelloApiView(APIView):
 
 	def delete(self, request, pk=None):
 		"""Delete and object"""
-		return Response({'method':'DELETE'})		
+		return Response({'method':'DELETE'})
 
-		
+class HelloViewSet(viewsets.ViewSet):
+	"""Test view set"""
+	serializer_class = serializers.HelloSerializer
+
+	def list(self, request):
+		"""Returns a hello message"""
+
+		a_viewset = [
+			'Uses actions (list, create, retrieve, update, partial_update)',
+			'Automatically maps to URLs using Routers',
+			'Provides more functionality with less code',
+		]
+
+		return Response({'message':'Hello!','a_viewset':a_viewset})
+
+
+	def create(self, request):
+		"""Creates a new hello message"""
+		serializer = self.serializer_class(data=request.data)
+
+		if serializer.is_valid():
+			name = serializer.validated_data.get('name')
+			message = 'Hello {name}'.format(name=name)
+			return Response({'message':message})
+		else:
+			return Response(
+				serializer.errors,
+				status = status.HTTP_400_BAD_REQUEST
+			)
+
+	def retrieve(self, request, pk=None):
+		"""Handle getting an object by it's id"""
+		return Response({'http_method':'GET'})
+
+	def update(self, request, pk=None):
+		"""Handle updating an object"""
+		return Response({'http_method':'PUT'})
+
+	def partial_update(self, request, pk=None):
+		"""Handle updating part of an object"""
+		return Response({'http_method':'PATCH'})
+
+	def destroy(self, request, pk=None):
+		"""Handle deleting an object"""
+		return Response({'http_method':'DELETE'})
